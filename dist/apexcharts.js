7d03b509e1fe9cde6269100c0f36e159764cae14
Kimtaehyeong <taehyeok02@gmail.com>
2024-11-28 22:05:10 +0900

    fix

--- dist/apexcharts.js
+++ dist/apexcharts.js
@@ -28154,8 +28154,8 @@
         var i = indexes.i;
         var j = indexes.j;
         var isPositive = true;
-        var colorPos = w.globals.seriesXvalues[j] > w.config.plotOptions.candlestick.colors.date[0] && w.globals.seriesXvalues[j] < w.config.plotOptions.candlestick.colors.date[1] ? w.config.plotOptions.candlestick.colors.upward[0] : w.config.plotOptions.candlestick.colors.upward[1];
-        var colorNeg = w.globals.seriesXvalues[j] > w.config.plotOptions.candlestick.colors.date[0] && w.globals.seriesXvalues[j] < w.config.plotOptions.candlestick.colors.date[1] ? w.config.plotOptions.candlestick.colors.downward[0] : w.config.plotOptions.candlestick.colors.downward[1];
+        var colorPos = w.globals.seriesXvalues[j] > w.config.plotOptions.candlestick.colors.date[0] && w.globals.seriesXvalues[j] < w.config.plotOptions.candlestick.colors.date[1] ? w.config.plotOptions.candlestick.colors.upward[1] : w.config.plotOptions.candlestick.colors.upward[0];
+        var colorNeg = w.globals.seriesXvalues[j] > w.config.plotOptions.candlestick.colors.date[0] && w.globals.seriesXvalues[j] < w.config.plotOptions.candlestick.colors.date[1] ? w.config.plotOptions.candlestick.colors.downward[1] : w.config.plotOptions.candlestick.colors.downward[0];
         var color = '';
         if (this.isBoxPlot) {
           color = [this.boxOptions.colors.lower, this.boxOptions.colors.upper];
